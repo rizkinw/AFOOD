f5f02338287c415bb599fd7ce519ddf3fd48d96a
rizkinw <32184661+rizkinw@users.noreply.github.com>
2018-01-07 14:37:51 +0700

    update connection.php

--- amikom/connection.php
+++ amikom/connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost"; //replace it with your database server name
 $username = "root";  //replace it with your database username
 $password = "";  //replace it with your database password
-$dbname = "amikom_event";
+$dbname = "afood";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
